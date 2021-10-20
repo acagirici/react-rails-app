f61dac3b131308bab623980fc761dae3d642f2e7
acagirici <acagirici23@gmail.com>
2021-10-19 20:56:46 -0400

    data strip

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -3,6 +3,7 @@ class Currency < ApplicationRecord
     def current_price
         url = "https://api.coinbase.com/v2/prices/"
         request = HTTParty.get(url + self.currency_symbol + "-usd/sell")
-        response = JSON.parse(request.body)
+        response = JSON.parse(request.body)["data"]["amount"]
+        # [0]["amount"]
     end
 end
