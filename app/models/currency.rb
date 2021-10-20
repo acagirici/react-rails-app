f5a1f7c4bcea341635d6f61ff58cc7d5183741fc
acagirici <acagirici23@gmail.com>
2021-10-19 22:00:00 -0400

    copleted rails side

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,9 +1,11 @@
 class Currency < ApplicationRecord
+    def calculate_value(amount)
+        current_price.to_f * amount.to_f
+    end
 
     def current_price
         url = "https://api.coinbase.com/v2/prices/"
-        request = HTTParty.get(url + self.currency_symbol + "-usd/sell")
+        request = HTTParty.get(url + self.currency_symbol + "-usd/buy")
         response = JSON.parse(request.body)["data"]["amount"]
-        # [0]["amount"]
     end
 end
