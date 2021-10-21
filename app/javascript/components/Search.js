2fbf0f67bd9559cb6d67c02ab3a101bd7e9ab208
acagirici <acagirici23@gmail.com>
2021-10-20 21:41:55 -0400

    display data to browser

--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -7,6 +7,8 @@ class Search extends Component {
     }
 
     render() {
+        const searchResults = this.props.searchResults.map(curr => 
+        <li key={curr.id}><a href="#"> <span>{curr.name}</span> <span>({curr.currency_symbol})</span> </a></li>)
         return(
             <div>
                 <h1>Cryptocurrency Calculator</h1>
@@ -14,6 +16,9 @@ class Search extends Component {
                     <div className="form-group">
                         <label>Search for your Currency</label><br/>
                         <input onChange={this.props.handleChange} type="text" name="name" placeholder="Ex: Bitcoin, Ethereum, Litecoin..." value={this.props.name} className="input-field"/>
+                    </div>
+                    <div className="currency-list">
+                        {searchResults}
                     </div>
                 </form>
             </div>
