cb0e3a3476e272b6b4006628eb44c79f779a385b
acagirici <acagirici23@gmail.com>
2021-12-12 21:54:17 -0500

    nav-bar component

---
+++ app/javascript/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const NavBar = (props) => {
+
+    return (
+        <div>
+            <Link to='/new_user'>New Account</Link>
+        </div>
+    )
+}
+
+export default NavBar
